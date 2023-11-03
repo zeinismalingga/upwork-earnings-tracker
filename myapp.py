fd4651e0b7535223f9c37193ca7b5520aa64a57a
zeinismalingga <zein.ismalingga@gmail.com>
2023-11-03 18:38:01 +0800

    add fee option

--- myapp.py
+++ myapp.py
@@ -15,13 +15,22 @@ if uploaded_file is not None:
     # Read the CSV file into a Pandas DataFrame
     df = pd.read_csv(uploaded_file)
 
-     # Filter the data by 'Fixed Price' and 'Hourly' in the 'type' column
+    # Display the uploaded data
+    # st.write("Uploaded Data:")
+    # st.write(df)
+
+    # Filter the data by 'Fixed Price' and 'Hourly' in the 'type' column
     filtered_df = df[df['Type'].isin(['Fixed Price', 'Hourly'])]
+    filtered_df_fee = df[df['Type'].isin(['Service Fee'])]
 
     # Extract the month from the 'date' column and sum the 'amount' for each month
     filtered_df['Date'] = pd.to_datetime(filtered_df['Date'])
     filtered_df['Month'] = filtered_df['Date'].dt.strftime('%b')
     monthly_summary = filtered_df.groupby('Month')['Amount'].sum().reset_index()
+
+    filtered_df_fee['Date'] = pd.to_datetime(filtered_df_fee['Date'])
+    filtered_df_fee['Month'] = filtered_df_fee['Date'].dt.strftime('%b')
+    monthly_summary_fee = filtered_df_fee.groupby('Month')['Amount'].sum().reset_index()
 
     # Create a custom sorting order for the 'Month' column
     month_order = [
@@ -31,35 +40,74 @@ if uploaded_file is not None:
     monthly_summary['Month'] = pd.Categorical(monthly_summary['Month'], categories=month_order, ordered=True)
     monthly_summary = monthly_summary.sort_values('Month')
 
-    # Display the uploaded data
-    # st.write("Uploaded Data:")
-    # st.write(df)
+    monthly_summary_fee['Month'] = pd.Categorical(monthly_summary_fee['Month'], categories=month_order, ordered=True)
+    monthly_summary_fee = monthly_summary_fee.sort_values('Month')
     
     st.markdown("---")
 
     # Display total 'Fixed Price' and 'Hourly' amounts
-    st.write("##### Total Earning: ", f"<span style='color:green;'>$ {filtered_df['Amount'].sum():,}</span>", unsafe_allow_html=True)
-    st.write("##### Charity (2.5%): ", f"<span style='color:red;'>$ {filtered_df['Amount'].sum() * 2.5 / 100:,}</span>", unsafe_allow_html=True)
+    # st.write("##### Total Earning: ", f"<span style='color:green;'>$ {filtered_df['Amount'].sum():,}</span>", unsafe_allow_html=True)
+    # st.write("##### Charity (2.5%): ", f"<span style='color:red;'>$ {filtered_df['Amount'].sum() * 2.5 / 100:,}</span>", unsafe_allow_html=True)
 
-    # Display sum of 'Fixed Price' and 'Hourly' amounts
-    # st.write(" #### Sum of Fixed Price and Hourly")
-    st.write(filtered_df.groupby('Type')['Amount'].sum())
+    # # Display sum of 'Fixed Price' and 'Hourly' amounts
+    # # st.write(" #### Sum of Fixed Price and Hourly")
+    # st.write(filtered_df.groupby('Type')['Amount'].sum())
 
-    # Create a bar chart for the monthly summary
-    st.write("##### Monthly Bar Chart")
-    fig, ax = plt.subplots()
-    bar_chart = ax.bar(monthly_summary['Month'], monthly_summary['Amount'])
+    # Add a radio button to select between "Included fee" and "Without fee" calculations
+    fee_option = st.radio("Choose fee option", ["Without fee", "Included fee"])
 
-    ax.set_xlabel('Month')
-    ax.set_ylabel('Amount ($)')
+    if fee_option == "Without fee":
+        # Display total 'Fixed Price' and 'Hourly' amounts with service fee
+        st.write("##### Total Earning: ", f"<span style='color:green;'>$ {filtered_df['Amount'].sum():,}</span>", unsafe_allow_html=True)
+        st.write("##### Charity (2.5%): ", f"<span style='color:red;'>$ {filtered_df['Amount'].sum() * 2.5 / 100:,}</span>", unsafe_allow_html=True)
 
-    ax.bar_label(bar_chart, fmt='{:,.0f}')
+        # Display sum of 'Fixed Price' and 'Hourly' amounts
+        st.write(filtered_df.groupby('Type')['Amount'].sum())
 
-    st.pyplot(fig)
+        # Create a bar chart for the monthly summary
+        st.write("##### Monthly Bar Chart")
+        fig, ax = plt.subplots()
+        bar_chart = ax.bar(monthly_summary['Month'], monthly_summary['Amount'])
 
-    # Display the monthly summary
-    st.write("##### Monthly Summary")
-    st.write(monthly_summary)
+        ax.set_xlabel('Month')
+        ax.set_ylabel('Amount ($)')
+
+        ax.bar_label(bar_chart, fmt='{:,.0f}')
+
+        st.pyplot(fig)
+
+        # Display the monthly summary
+        st.write("##### Monthly Summary")
+        st.write(monthly_summary)
+
+    elif fee_option == "Included fee":
+        # Calculate the total amount without service fee
+        total_earning_included_fee = filtered_df['Amount'].sum() - abs(df[df['Type'].isin(['Service Fee'])].sum()['Amount'])
+        st.write("##### Total Earning: ", f"<span style='color:green;'>$ {total_earning_included_fee:,}</span>", unsafe_allow_html=True)
+        charity_percentage = 2.5
+        st.write("##### Charity (2.5%): ", f"<span style='color:red;'>$ {total_earning_included_fee * charity_percentage / 100:.2f}</span>", unsafe_allow_html=True)
+        st.write("##### Total Fee: ", f"<span style='color:red;'>$ {abs(df[df['Type'].isin(['Service Fee'])]['Amount'].sum()):.2f}</span>", unsafe_allow_html=True)
+
+
+        # Display sum of 'Fixed Price' and 'Hourly' amounts
+        # st.write(filtered_df.groupby('Type')['Amount'].sum() - abs(df[df['Type'] == 'Service Fee']['Amount'].sum()))
+        # st.write(abs(df[df['Type'] == 'Service Fee']['Amount'].sum()))
+
+        # Create a bar chart for the monthly summary
+        st.write("##### Monthly Bar Chart")
+        fig, ax = plt.subplots()
+        bar_chart = ax.bar(monthly_summary['Month'], monthly_summary['Amount'] - abs(monthly_summary_fee['Amount']))
+
+        ax.set_xlabel('Month')
+        ax.set_ylabel('Amount ($)')
+
+        ax.bar_label(bar_chart, fmt='{:,.0f}')
+
+        st.pyplot(fig)
+
+        # Display the monthly summary
+        # st.write("##### Monthly Summary")
+        # st.write(monthly_summary)
 
 st.markdown("---")
 st.markdown("Made with ❤️ by [Zein](https://www.upwork.com/freelancers/zeini)")
